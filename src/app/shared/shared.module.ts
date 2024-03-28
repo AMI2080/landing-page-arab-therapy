0b7be939623b16e03361684bfefe8e285d362c62
AMI2080 <AMI_2080@yahoo.com>
2024-03-28 16:36:49 +0200

    start using translation

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,38 @@ import { CommonModule } from '@angular/common';
 import { PageComponent } from './page/page.component';
 import { HeaderComponent } from './page/header/header.component';
 import { FooterComponent } from './page/footer/footer.component';
+import { HttpClient, provideHttpClient, withFetch } from '@angular/common/http';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 const sharedComponents = [PageComponent, HeaderComponent, FooterComponent];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    TranslateModule.forRoot({
+      defaultLanguage: 'ar',
+      useDefaultLang: true,
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   declarations: [
     // components
     ...sharedComponents,
   ],
   exports: [
+    TranslateModule,
     // components
     ...sharedComponents,
   ],
+  providers: [provideHttpClient(withFetch())],
 })
 export class SharedModule {}
